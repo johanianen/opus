c7c8734f0cf3541859db33d727c61089cc7c8868
johanianen <120122757+johanianen@users.noreply.github.com>
2026-09-10 10:56:32 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -17,33 +17,32 @@ let in_setup = 1
 basic.showNumber(player_number)
 basic.forever(function () {
     while (in_setup) {
-    	
-    }
-    if (input.buttonIsPressed(Button.AB)) {
-        player = player_number
-        in_setup = 0
-        for (let index = 0; index < 4; index++) {
-            basic.showIcon(IconNames.Square)
-            basic.pause(100)
-            basic.showNumber(player)
-            basic.pause(100)
-        }
-        has_dice_1 = 1
-    } else if (input.buttonIsPressed(Button.B)) {
-        player_number += 1
-        if (player_number > 7) {
-            player_number = 1
-        }
-        basic.showNumber(player_number)
-    } else {
-        if (input.buttonIsPressed(Button.A)) {
-            player_number += -1
-            if (player_number < 1) {
-                player_number = 7
+        if (input.buttonIsPressed(Button.AB)) {
+            player = player_number
+            in_setup = 0
+            for (let index = 0; index < 4; index++) {
+                basic.showIcon(IconNames.Square)
+                basic.pause(100)
+                basic.showNumber(player)
+                basic.pause(100)
+            }
+            has_dice_1 = 1
+        } else if (input.buttonIsPressed(Button.B)) {
+            player_number += 1
+            if (player_number > 7) {
+                player_number = 1
             }
             basic.showNumber(player_number)
         } else {
-            continue;
+            if (input.buttonIsPressed(Button.A)) {
+                player_number += -1
+                if (player_number < 1) {
+                    player_number = 7
+                }
+                basic.showNumber(player_number)
+            } else {
+                continue;
+            }
         }
     }
 })
