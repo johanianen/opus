c3228adfa8fbc7cc26915937d296cd6e0b73673b
johanianen <120122757+johanianen@users.noreply.github.com>
2026-09-10 10:55:53 +0200

    la in "a+b låser spelarnummer" i forever

--- main.ts
+++ main.ts
@@ -1,14 +1,3 @@
-input.onButtonPressed(Button.AB, function () {
-    player = player_number
-    in_setup = 0
-    for (let index = 0; index < 4; index++) {
-        basic.showIcon(IconNames.Square)
-        basic.pause(100)
-        basic.showNumber(player)
-        basic.pause(100)
-    }
-    has_dice_1 = 1
-})
 input.onGesture(Gesture.Shake, function () {
     let has_dice_2 = 0
     if (has_dice_1 || has_dice_2) {
@@ -22,24 +11,39 @@ input.onGesture(Gesture.Shake, function () {
 let send_number = 0
 let dice_number = 0
 let has_dice_1 = 0
-let in_setup = 0
+let player_number = 1
 let player = 0
-let player_number = 0
-player_number = 0
-player = 0
-in_setup = 1
+let in_setup = 1
+basic.showNumber(player_number)
 basic.forever(function () {
     while (in_setup) {
-        if (input.buttonIsPressed(Button.B)) {
-            player_number += 1
+    	
+    }
+    if (input.buttonIsPressed(Button.AB)) {
+        player = player_number
+        in_setup = 0
+        for (let index = 0; index < 4; index++) {
+            basic.showIcon(IconNames.Square)
+            basic.pause(100)
+            basic.showNumber(player)
+            basic.pause(100)
+        }
+        has_dice_1 = 1
+    } else if (input.buttonIsPressed(Button.B)) {
+        player_number += 1
+        if (player_number > 7) {
+            player_number = 1
+        }
+        basic.showNumber(player_number)
+    } else {
+        if (input.buttonIsPressed(Button.A)) {
+            player_number += -1
+            if (player_number < 1) {
+                player_number = 7
+            }
             basic.showNumber(player_number)
         } else {
-            if (input.buttonIsPressed(Button.A)) {
-                player_number += -1
-                basic.showNumber(player_number)
-            } else {
-                continue;
-            }
+            continue;
         }
     }
 })
